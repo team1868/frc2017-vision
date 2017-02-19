e0a16a6471c6d801aded531709cdea24691ab387
Maggie Wang <maggi3wang@gmail.com>
2017-02-18 19:50:58 -0800

    Peg detection works on KOP bot. More tuning for high goal

--- src/HighGoalDetection/HighGoalDetect.cpp
+++ src/HighGoalDetection/HighGoalDetect.cpp
@@ -20,17 +20,18 @@ using namespace std;
 #define PI 3.14159265
 
 //FIELD SPECS
-const int DIST_BETWEEN_GOAL_TARGETS = 7; //inches
-const int TOP_TARGET_HEIGHT = 88; //inches, top of tape
-const int BOTTOM_TARGET_HEIGHT = 78; //inches, bottom of tape
+const double DIST_BETWEEN_GOAL_TARGETS = 7.0; //inches
+const double TOP_TARGET_HEIGHT = 88.0; //inches, top of tape
+const double BOTTOM_TARGET_HEIGHT = 78.0; //inches, bottom of tape
+const double WIDTH_OF_GOAL = 15.0; //inches
 
 //CAMERA SPECS
 const double DIAGONAL_FOV = 68.5; //degrees
 const double HORIZONTAL_FOV = 45.0; //degrees was 61.39
 const double VERTICAL_FOV = 36.0; //degrees
-const double FOCAL_LENGTH = 539.0485745586101; //pixelzzz
-const int PIXEL_WIDTH = 640; //pixelzzz
-const int PIXEL_HEIGHT = 480; //pixelzzz
+const double FOCAL_LENGTH = 239.369; //pixelzzz was 539.0485745586101
+const double PIXEL_WIDTH = 640.0; //pixelzzz
+const double PIXEL_HEIGHT = 480.0; //pixelzzz
 const double CENTER_LINE = 319.5; //pixelzzz
 //CHECK BELOW
 const double CAMERA_ANGLE = 0.0; //degrees
@@ -39,120 +40,126 @@ const double OFFSET_TO_FRONT = 0.0; //inches
 
 string WINDOW_NAME = "High Goal Window";
 
-const int LOWER_GREEN_HUE = 30;
-const int LOWER_GREEN_SAT = 60; 
-const int LOWER_GREEN_VAL = 60;
+const int LOWER_GREEN_HUE = 30;  // 30;
+const int LOWER_GREEN_SAT = 100;  // 60 
+const int LOWER_GREEN_VAL = 60;  // 60
 
-const int UPPER_GREEN_HUE = 130;
-const int UPPER_GREEN_SAT = 255;
-const int UPPER_GREEN_VAL = 255;
+const int UPPER_GREEN_HUE = 130; // 130
+const int UPPER_GREEN_SAT = 255; // 255
+const int UPPER_GREEN_VAL = 255; // 255
 
 const double CAP_SAT = 0.5;
 
-const int MIN_AREA = 3000; //was 2000
+const int MIN_AREA = 2000; //was 2000
 RNG rng(12345);
 
 int main() {
-	cout << "Built with OpenCV B-) " << CV_VERSION << endl;
-	Mat image;
-	VideoCapture capture(1);
-//	capture.set(CV_CAP_PROP_EXPOSURE, 0.0);
-	capture.set(CV_CAP_PROP_SATURATION, CAP_SAT);
+  cout << "Built with OpenCV B-) " << CV_VERSION << endl;
+  Mat image;
+  VideoCapture capture(2);
+//  capture.set(CV_CAP_PROP_EXPOSURE, 0.0);
+  capture.set(CV_CAP_PROP_SATURATION, CAP_SAT);
 
- 	if (!capture.isOpened()) {
-	    	cout << "!!! Failed to open camera darn :((((( " << endl;
-    		return -1;
-  	}
+  if (!capture.isOpened()) {
+    cout << "!!! Failed to open camera darn :((((( " << endl;
+    return -1;
+  }
 
-	Mat frame;
+  Mat frame;
+  //Prepare our context and publisher
+  context_t context(1);
+  socket_t publisher(context, ZMQ_PUB);
+  publisher.bind("tcp://*:5563");
 
-	for(;;) {
+  for(;;) {
 //READING
-		if (!capture.read(frame)) {
-			break;
-		}
+    if (!capture.read(frame)) {
+      break;
+    }
 
-		Mat cloneImage = frame.clone();
-		imshow(WINDOW_NAME, cloneImage);
+    Mat cloneImage = frame.clone();
+    //imshow(WINDOW_NAME, cloneImage);
 //STARTING PROCESSING
-		medianBlur(frame,frame, 5);
+    medianBlur(frame,frame, 5);
 
-		Mat hsvImage;
-		cvtColor(frame, hsvImage, COLOR_BGR2HSV);
+    Mat hsvImage;
+    cvtColor(frame, hsvImage, COLOR_BGR2HSV);
 
-		Mat greenRange;
-		inRange(hsvImage, Scalar(LOWER_GREEN_HUE, LOWER_GREEN_SAT, LOWER_GREEN_VAL), Scalar(UPPER_GREEN_HUE, UPPER_GREEN_SAT, UPPER_GREEN_VAL), greenRange);
+    Mat greenRange;
+    inRange(hsvImage, Scalar(LOWER_GREEN_HUE, LOWER_GREEN_SAT, LOWER_GREEN_VAL), Scalar(UPPER_GREEN_HUE, UPPER_GREEN_SAT, UPPER_GREEN_VAL), greenRange);
 
-		GaussianBlur(greenRange, greenRange, Size(9,9), 2,2);
+    GaussianBlur(greenRange, greenRange, Size(9,9), 2,2);
 
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-		findContours (greenRange, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
-		vector<vector<Point> > contours_poly(contours.size() );
-		vector<Rect> boundRect(contours.size() );
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours (greenRange, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
+    vector<vector<Point> > contours_poly(contours.size() );
+    vector<Rect> boundRect(contours.size() );
 
-		int upRect;
-		int downRect;
+    int upRect;
+    int downRect;
 
-		for(int c = 0; c < contours.size(); c++) {
-			Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) );
-			if (contourArea(contours[c]) < MIN_AREA) {
-				continue;
-			}
-			drawContours(cloneImage, contours, c, color, 2, 8, hierarchy, 0, Point() );
-			approxPolyDP( Mat(contours[c]), contours_poly[c], 3, true);
-			boundRect[c] = boundingRect( Mat(contours_poly[c]) );
-			//double aspectRatio = (boundRect[c].br().x - boundRect[c].tl().x)/(boundRect[c].br().y - boundRect[c].tl().y);
+    for(int c = 0; c < contours.size(); c++) {
+      Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255) );
+      if (contourArea(contours[c]) < MIN_AREA) {
+        continue;
+      }
+      drawContours(cloneImage, contours, c, color, 2, 8, hierarchy, 0, Point() );
+      approxPolyDP( Mat(contours[c]), contours_poly[c], 3, true);
+      boundRect[c] = boundingRect( Mat(contours_poly[c]) );
+      //double aspectRatio = (boundRect[c].br().x - boundRect[c].tl().x)/(boundRect[c].br().y - boundRect[c].tl().y);
 //Be careful for aspectRatio
-			rectangle(cloneImage, boundRect[c].tl(), boundRect[c].br(), color, 2, 8, 0);
+       rectangle(cloneImage, boundRect[c].tl(), boundRect[c].br(), color, 2, 8, 0);
+    }
+    
+    for(int c = 0; c < boundRect.size();) {
+//       cout << "bound rect area: " << boundRect[c].area() << endl;
+      if (boundRect[c].area() < MIN_AREA) {
+        boundRect.erase(boundRect.begin() + c);
+        continue;
+      }
+      c++;
+    }
+    
+    if (boundRect.size() < 2) {
+      cout << "no rectangles :(" << endl;
+//      continue;
+    }
 
-		}
-		
-		for(int c = 0; c < boundRect.size();) {
-			cout << "bound rect area: " << boundRect[c].area() << endl;
-			if (boundRect[c].area() < MIN_AREA) {
-				boundRect.erase(boundRect.begin() + c);
-				continue;
-			}
-			c++;
-		}
-		
-		if (boundRect.size() < 2) {
-			cout << "no rectangles :(" << endl;
-			continue;
-		}
-
-		if (boundRect[0].br().y < boundRect[1].br().y) {
-			upRect = 0;
-			downRect = 1;
-		} else {
-			upRect = 1;
-			downRect = 0;
-		}
-		double centerOfTargets = (boundRect[upRect].tl().x + boundRect[downRect].br().x) / 2.0;
+    if (boundRect[0].br().y < boundRect[1].br().y) {
+      upRect = 0;
+      downRect = 1;
+    } else {
+      upRect = 1;
+      downRect = 0;
+    }
+    //double centerOfTargets = (boundRect[upRect].tl().x + boundRect[downRect].br().x) / 2.0;
+    double centerOfTargets = (boundRect[upRect].tl().x + boundRect[upRect].br().x) / 2.0;
 //FINDING ANGLE
-		double angleToMoveApprox;
-		angleToMoveApprox = (centerOfTargets - CENTER_LINE) * HORIZONTAL_FOV / PIXEL_WIDTH;
-		cout << "approx angle: " << angleToMoveApprox << endl;
-		cout << "pixel distance for centers: " << centerOfTargets - CENTER_LINE << endl;
+    double angleToMoveApprox;
+    angleToMoveApprox = (centerOfTargets - CENTER_LINE) * HORIZONTAL_FOV / PIXEL_WIDTH;
+    cout << "approx angle: " << angleToMoveApprox << endl;
+    cout << "pixel distance for centers: " << centerOfTargets - CENTER_LINE << endl;
 
-		double angleToMoveAgain;
-		angleToMoveAgain = atan((centerOfTargets - CENTER_LINE) / FOCAL_LENGTH);
-		angleToMoveAgain = (angleToMoveAgain *180/PI); //converts radients to degrees
-		cout << "angleToMoveAgain: " << angleToMoveAgain << endl;
+     double angleToMoveAgain;
+    angleToMoveAgain = atan((centerOfTargets - CENTER_LINE) / FOCAL_LENGTH);
+    angleToMoveAgain = (angleToMoveAgain *180/PI); //converts radients to degrees
+    cout << "angleToMoveAgain: " << angleToMoveAgain << endl;
 
-		double distanceToTopOfGoal;
-		double y = boundRect[downRect].br().y + (boundRect[upRect].tl().y - boundRect[downRect].br().y) / 2.0;
-		y = -(2 *y / PIXEL_HEIGHT -1);
-		distanceToTopOfGoal = (TOP_TARGET_HEIGHT - CAMERA_HEIGHT) / tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * PI / 180);
-		cout << "distance to top goal: " << distanceToTopOfGoal << endl;
+    double distanceToHighGoal;
+    double pixelWidthOfTargets = boundRect[upRect].br().x - boundRect[upRect].tl().x;
+    //double y = boundRect[downRect].br().y + (boundRect[upRect].tl().y - boundRect[downRect].br().y) / 2.0;
+    //y = -(2 *y / PIXEL_HEIGHT -1);
+    //distanceToTopOfGoal = (TOP_TARGET_HEIGHT - CAMERA_HEIGHT) / tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * PI / 180);
+    distanceToHighGoal = WIDTH_OF_GOAL * FOCAL_LENGTH / pixelWidthOfTargets;
+    cout << "distance to high goal: " << distanceToHighGoal << endl;
 
-		imshow(WINDOW_NAME, cloneImage);
+    imshow(WINDOW_NAME, cloneImage);
 
-		char key = cvWaitKey (10); 
-		if (key == 27) { //Esc
-			break;
-		}
-	}
-	return 0;
+    char key = cvWaitKey (10); 
+    if (key == 27) { //Esc
+      break;
+    }
+  }
+  return 0;
 }
--- src/PegDetection/PegDetect.cpp
+++ src/PegDetection/PegDetect.cpp
@@ -32,12 +32,12 @@ const double BOTTOM_PEG_TARGET_HEIGHT = 10.75; //inches, bottom of tape
 
 //CAMERA SPECS
 const double DIAGONAL_FOV = 68.5; //degrees
-const double HORIZONTAL_FOV = 45.0; // was 52.034 degrees, originally 61.39, was 35 for 640x480
+const double HORIZONTAL_FOV = 60.0; //45.0; // was 52.034 degrees, originally 61.39, was 35 for 640x480 45.0 <-- was decnt
 const double VERTICAL_FOV = 40.32; //degrees was 35 was 39.2
-const double FOCAL_LENGTH = 539.0485; //1078.09714 539.0485745586101 pixelzzz
-const int PIXEL_WIDTH = 640; // was 1280 pixelzzz was 640
-const int PIXEL_HEIGHT = 480; // was 720 pixelzzz was 480
-const double CENTER_LINE = 319.5; //639.5 pixelzzz was 319.5
+const double FOCAL_LENGTH = 1170.0;  // was 1197.097; //1078.09714 539.0485745586101 pixelzzz 365.704 704.389
+const int PIXEL_WIDTH = 1280; // was 1280 pixelzzz was 640
+const int PIXEL_HEIGHT = 720; // was 720 pixelzzz was 480
+const double CENTER_LINE = 639.5; //639.5 pixelzzz was 319.5
 //CHECK BELOW
 const double CAMERA_ANGLE = 0.0; //degrees
 const double CAMERA_HEIGHT = 6.0; //inches, was 24
@@ -62,8 +62,8 @@ int main()
   Mat image;
   VideoCapture capture(1);
   capture.set(CV_CAP_PROP_SATURATION, 0.5);
-//  capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);
-//  capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);
+  capture.set(CV_CAP_PROP_FRAME_WIDTH,1280);
+  capture.set(CV_CAP_PROP_FRAME_HEIGHT,720);
 //  cout << "width " << capture.get(CV_CAP_PROP_FRAME_WIDTH);
 //  cout << "height  " << capture.get(CV_CAP_PROP_FRAME_HEIGHT);
 
@@ -150,7 +150,9 @@ int main()
       leftContour = 1;
       rightContour = 0;
     }
-    double centerOfTargets = (boundRect[leftContour].tl().x + boundRect[rightContour].br().x) / 2.0;
+//    double centerOfTargets = (boundRect[leftContour].tl().x + boundRect[rightContour].br().x) / 2.0;
+//    double centerOfTargets = (((boundRect[leftContour].bl().x + boundRect[leftContour].br().x) / 2.0) + ((boundRect[rightContour].bl().x + boundRect[rightContour].br().x))/2.0;
+    double centerOfTargets = (((boundRect[leftContour].tl().x + boundRect[leftContour].br().x) / 2.0) + ((boundRect[rightContour].tl().x + boundRect[rightContour].br().x) / 2.0)) / 2.0;
 
 //FINDING ANGLE
     double angleToMoveApprox;
@@ -174,22 +176,27 @@ int main()
     double y = rectForCalc.br().y + rectForCalc.height / 2.0;
 //    y = -(2 * y / PIXEL_HEIGHT - 1);
 //    distanceToPeg = (TOP_PEG_TARGET_HEIGHT - CAMERA_HEIGHT) / (tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * PI / 180.0));
-    double apparentWidth = boundRect[leftContour].tl().x - boundRect[rightContour].br().x;
-    double distortionFactor = 1.020;
-    distanceToPeg = (DIST_BETWEEN_PEG_TARGETS) / (tan(distortionFactor*apparentWidth/PIXEL_WIDTH*HORIZONTAL_FOV));
+    double apparentWidth = boundRect[rightContour].br().x - boundRect[leftContour].tl().x;
+    distanceToPeg = DIST_BETWEEN_PEG_TARGETS * FOCAL_LENGTH / apparentWidth;
+//    double distortionFactor = 1.020;
+//    distanceToPeg = (DIST_BETWEEN_PEG_TARGETS) / (tan(distortionFactor*apparentWidth/PIXEL_WIDTH*HORIZONTAL_FOV));
     cout << "hallo distanc " << distanceToPeg << endl;
 
-    imshow(WINDOW_NAME, origImage);
+//    imshow(WINDOW_NAME, origImage);
     //imshow("suh", frame);
     //imshow("yooo green", greenRange);
     //imshow("yooo gray", grayImage);
 
     //  Write two messages, each with an envelope and content
-    s_sendmore (publisher, "A");
-    s_send (publisher, "We don't want to see this");
-    s_sendmore (publisher, "B");
+//    s_sendmore (publisher, "A");
+//    s_send (publisher, "We don't want to see this");
+    s_sendmore (publisher, "ANGLE");
        string pub_string_approx = to_string(angleToMoveApprox);
     s_send (publisher, pub_string_approx);
+
+    s_sendmore (publisher, "DISTANCE");
+       string another_string = to_string(distanceToPeg);
+    s_send (publisher, another_string);
     //s_send (publisher, i);
 //    this_thread::sleep_for(chrono::milliseconds(1));
 
